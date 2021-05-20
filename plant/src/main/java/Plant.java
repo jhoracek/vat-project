d53beb0bc86dfb293b060c1abc0787190899ac31
Jiri Horacek <horymail@seznam.cz>
2021-05-20 07:56:56 +0200

    plant homework

--- plant/src/main/java/Plant.java
+++ plant/src/main/java/Plant.java
@@ -1,28 +1,73 @@
+import java.text.ParseException;
 import java.time.LocalDate;
 
 public class Plant {
 
+    static final int DEFAULT_FREQ = 7;
     private String name;
+    private String notes;
     private LocalDate planted;
-    private LocalDate watering;
-    private int freqOfWatering;
+    private LocalDate lastWatering;
+    private int freqOfWateringInDays;
 
-
-    public void getWateringInfo() {
-        System.out.println(name + watering + freqOfWatering);
-    }
-
-    public Plant(String name, LocalDate planted, LocalDate watering, int freqOfWatering) {
+    public Plant(String name, String notes, LocalDate planted, LocalDate lastWatering, int freqOfWateringInDays) {
         this.name = name;
+        this.notes = notes;
         this.planted = planted;
-        this.watering = watering;
-        this.freqOfWatering = freqOfWatering;
+        this.lastWatering = lastWatering;
+        this.freqOfWateringInDays = freqOfWateringInDays;
     }
 
-    public Plant(String name, LocalDate watering) {
-        this.name = name;
-        this.watering = watering.now();
+    public Plant(String name, LocalDate planted, int freqOfWateringInDays) {
+        this(name, "", planted, LocalDate.now(), freqOfWateringInDays);
     }
+
+    public Plant(String name) {
+        this(name, LocalDate.now(), DEFAULT_FREQ);
+    }
+
+    public String getWateringInfo() {
+        return getName() + ", posledni zalivka:" + getlastWatering() + " " + getlastWatering()
+                .plusDays(getfreqOfWateringInDays());
+    }
+
+    public String prepareOutputString(String delimiter) {
+        return
+                getName() + delimiter
+                        + getNotes() + delimiter
+                        + getfreqOfWateringInDays() + delimiter
+                        + getlastWatering() + delimiter
+                        + getPlanted();
+    }
+
+    public int getFreqOfWateringInDays() {
+        return freqOfWateringInDays;
+    }
+
+    public LocalDate getLastWatering() {
+        return lastWatering;
+    }
+
+    public void setFreqOfWateringInDays(int freqOfWateringInDays) {
+        this.freqOfWateringInDays = freqOfWateringInDays;
+    }
+
+    public void setLastWatering(LocalDate lastWatering) {
+        this.lastWatering = lastWatering;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
 
     public void setName(String name) {
         this.name = name;
@@ -32,33 +77,46 @@ public class Plant {
         this.planted = planted;
     }
 
-    public void setWatering(LocalDate watering) {
-        this.watering = watering;
+    public void setlastWatering(LocalDate lastWatering) throws PlantException {
+        if (lastWatering.isBefore(getPlanted())) {
+            throw new PlantException("Last watering day: " + lastWatering + " cannot be less than day " +
+                    "of its planting.");
+        }
+        this.lastWatering = lastWatering;
     }
 
-    public void setFreqOfWatering(int freqOfWatering) {
-        this.freqOfWatering = freqOfWatering;
-    }
-
-    public String getName() {
-        return name;
+    public void setfreqOfWateringInDays(int freqOfWateringInDays) throws PlantException {
+        if (freqOfWateringInDays <= 0) throw new PlantException("Allowed frequency should be at least 1 day. " +
+                "Provided" + freqOfWateringInDays + " is not accepted. Please fix it.");
+        this.freqOfWateringInDays = freqOfWateringInDays;
     }
 
     public LocalDate getPlanted() {
         return planted;
     }
 
-    public LocalDate getWatering() {
-        return watering;
+    public LocalDate getlastWatering() {
+        return lastWatering;
     }
 
-    public int getFreqOfWatering() {
-        return freqOfWatering;
+    public int getfreqOfWateringInDays() {
+        return freqOfWateringInDays;
     }
 
-    public Plant(LocalDate planted, LocalDate watering, int freqOfWatering) {
-        this.planted = planted;
-        this.watering = watering;
-        this.freqOfWatering = freqOfWatering;
+    public static Plant parse(String text, String delimiter) throws ParseException {
+        String[] items = text.split(delimiter);
+
+        int numberOfItems = items.length;
+        if (numberOfItems != 5)
+            throw new ParseException("Nesprávný počet položek na řádku! Očekáváme 5 položek, místo " + numberOfItems + " položek na řádku: " + text, 0);
+
+        String name = items[0];
+        String notes = items[1];
+        int freqOfWateringInDays = Integer.parseInt(items[2]);
+        LocalDate lastWatering = LocalDate.parse(items[3]);
+        LocalDate planted = LocalDate.parse(items[4]);
+
+        return new Plant(name, notes, planted, lastWatering, freqOfWateringInDays);
     }
 }
+
--- plant/src/main/java/PlantException.java
+++ plant/src/main/java/PlantException.java
@@ -1,2 +1,5 @@
 public class PlantException extends Exception {
+    public PlantException(String message) {
+        super(message);
+    }
 }
