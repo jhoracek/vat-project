55c548ec2771559232db43fdf43c82e1c7942b32
Jiri Horacek <horymail@seznam.cz>
2021-05-24 08:56:44 +0200

    Array used for stateCodes under line

--- vat-project/src/main/java/Main.java
+++ vat-project/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
     private static final String INPUT_FILE = "vat-eu.csv";
@@ -12,18 +15,25 @@ public class Main {
             e.printStackTrace();
         }
 
+        String textBelowLine = "";
         for (int i = 0; i < list.size(); i++) {
             if (list.getVatList(i).getStdVat() > 20 && !list.getVatList(i).isSpecVat()) {
                 System.out.println(list.getVatList(i).getStateName() + " (" + list.getVatList(i).getStateCode() +
                         "): " + list.getVatList(i).getStdVat() + "%");
+            } else {
+
+                textBelowLine = textBelowLine + " " + list.getVatList(i).getStateCode();
+
+
             }
         }
+        String[] codesUnderLine = textBelowLine.split(" ");
+        Arrays.sort(codesUnderLine);
+        String sortedCodes = Arrays.toString(codesUnderLine);
+
+
         System.out.println("====================");
-        for (int i = 0; i < list.size(); i++) {
-            if (list.getVatList(i).getStdVat() <= 20 || list.getVatList(i).isSpecVat()) {
-                System.out.println("VAT Rate 20 % and less or they use special VAT rate:"+
-                        list.getVatList(i).getStateName());
-            }
-        }
+        System.out.println("Sazba VAT 20 % nebo nižší nebo používají speciální sazbu: "+
+                Arrays.toString(codesUnderLine));
     }
 }
