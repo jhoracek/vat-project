dd16d3c093b78f927121cd0f672a9d0589cd35aa
jhoracek <horymail@seznam.cz>
2021-06-09 16:54:21 +0200

    excercise for Unit tests and GitHub Actions

---
+++ lekce7-mvn-a-unit-testy/src/test/java/MujTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class MujTest {
+    @Test
+    public void PrvniTest() {
+
+    }
+
+    @Test
+    public void druhyTest() {
+        MojeTrida mojeTrida = new MojeTrida();
+        mojeTrida.run();
+    }
+}
